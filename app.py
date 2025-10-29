5208abb5ef83a2a1189f13cacf1a50649f7167c5
cutosb <boris.cutos@fisagrp.com>
2025-10-28 23:02:42 -0500

    startup 2

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app_b.py
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,6 +5,18 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def hello_world():
     return 'Hola desde app B', 200
+
+@app.route('/health', methods=['GET'])
+def call_health():
+    return 'OK', 200
+
+@app.route('/startup', methods=['GET'])
+def call_startup():
+    return 'OK', 200
+    
+@app.route('/readiness', methods=['GET'])
+def call_readiness():
+    return 'OK', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
